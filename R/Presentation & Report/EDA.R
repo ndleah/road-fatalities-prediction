927a35d5fff1782def3cae1119d2c76adc3571e6
Leah Nguyen <nduongthucanh@gmail.com>
2021-09-12 15:02:15 +0700

    Update EDA.R

--- R/Presentation & Report/EDA.R
+++ R/Presentation & Report/EDA.R
@@ -2,121 +2,215 @@ library(tidyverse) #data manipulation
 library(here)
 library(dplyr)
 library(ggplot2)
+library(patchwork)
 
 # Load the dataset
-accidents <- read_csv(here('data', 'Car_Accident_Data.csv'))
+accidents <- read_csv(here('data', 'car_accident.csv'))
+
+#--------------------------------------------------------------------------------------------
+# Data Wrangling
+#--------------------------------------------------------------------------------------------
+
 
 # get column names
-colnames(accidents)
-
-# Data Wrangling
-# View Columns in batches
-df[,1:18]
-df[,19:36]
-df[,37:51]
-
-# Data overview
+colnames(accidents)  
+unique(accidents$Inj_Level_Desc)
 dim(accidents)
+ 
+# filter only rows that have fatality
+fatality_accidents <- accidents %>% filter(Inj_Level_Desc=="Fatality")
+dim(fatality_accidents)
+
+# add columns for Accident hours
+fatality_accidents <- format(strptime(fatality_accidents$ACCIDENTTIME, "%H:%M"), "%H:00")
+
+# order Day Of Week chronically
+fatality_accidents$Day_Week_Description <- ordered( 
+  fatality_accidents$Day_Week_Description, levels=c("Monday", "Tuesday", "Wednesday", 
+                                                    "Thursday", "Friday", "Saturday", "Sunday")
+)
+
 
 # Check for NA values
 colSums(is.na(accidents))
 
-# Drop NA values
-## accidents <- accidents[complete.cases(accidents), ]
-## accidents <- na.omit(accidents)
 
 #--------------------------------------------------------------------------------------------
 # Data Visualization
 #--------------------------------------------------------------------------------------------
 
-accidents$Day_Week_Description <- ordered( # order Day Of Week chronically
-  accidents$Day_Week_Description, levels=c("Monday", "Tuesday", "Wednesday", 
-                                           "Thursday", "Friday", "Saturday", 
-                                           "Sunday")
+#----------------------------------------------------------
+# Visualization 1: Number of Fatality During Day of Weeks
+#----------------------------------------------------------
+weekdays <- ggplot(fatality_accidents) +
+            aes(x = Day_Week_Description) +
+            geom_bar(fill = "#B22222") +
+            labs(
+              x = "Weekdays",
+              y = "# of death",
+              title = "Number of Fatality During Day of Weeks"
+            ) +
+            ggthemes::theme_tufte() +
+            theme(
+              plot.title = element_text(size = 15L,
+                                        face = "bold",
+                                        hjust = 0.5),
+              plot.subtitle = element_text(size = 17L,
+                                           hjust = 0.5),
+              axis.title.y = element_text(size = 12L),
+              axis.title.x = element_text(size = 12L)
+            )
+weekdays
+
+
+#----------------------------------------------------------
+# Visualization 2: Number of Fatality During Day Hours
+#----------------------------------------------------------
+hours <-   ggplot(fatality_accidents) +
+            aes(x = ACCIDENT_HOUR) +
+            geom_bar(fill = "#B22222") +
+            labs(
+              x = "Hours",
+              y = "# of death",
+              title = "Number of Fatality During Day Hours"
+            ) +
+            ggthemes::theme_tufte() +
+            theme(
+              plot.title = element_text(size = 15L,
+                                        face = "bold",
+                                        hjust = 0.5),
+              plot.subtitle = element_text(size = 17L,
+                                           hjust = 0.5),
+              axis.title.y = element_text(size = 12L),
+              axis.title.x = element_text(size = 12L)
+            )
+hours
+
+
+#----------------------------------------------------------
+# Visualization 3: Number of Fatality During Weekend Vs. In week Hours
+#----------------------------------------------------------
+# weekend hours
+weekend_hours <- fatality_accidents %>%
+                filter(Day_Week_Description %in% c("Saturday", "Sunday")) %>%
+                ggplot() +
+                aes(x = ACCIDENT_HOUR) +
+                geom_bar(fill = "#FF0000") +
+                labs(
+                  x = "Weekdays",
+                  y = "# of death",
+                  title = "Number of Fatality During Weekend Hours"
+                ) +
+                coord_flip() +
+                ggthemes::theme_tufte() +
+                theme(
+                  plot.title = element_text(size = 15L,
+                                            face = "bold",
+                                            hjust = 0.5),
+                  plot.subtitle = element_text(size = 17L,
+                                               hjust = 0.5),
+                  axis.title.y = element_text(size = 12L),
+                  axis.title.x = element_text(size = 12L)
+                )
+# in-week hours
+inweek_hours <- fatality_accidents %>%
+                filter(!(Day_Week_Description %in% c("Sunday", "Saturday"))) %>%
+                ggplot() +
+                aes(x = ACCIDENT_HOUR) +
+                geom_bar(fill = "#F45555") +
+                labs(
+                  x = "Hours",
+                  y = "# of death",
+                  title = "Number of Fatality During In-week Hours"
+                ) +
+                coord_flip() +
+                ggthemes::theme_tufte() +
+                theme(
+                  plot.title = element_text(size = 15L,
+                                            face = "bold",
+                                            hjust = 0.5),
+                  plot.subtitle = element_text(size = 17L,
+                                               hjust = 0.5),
+                  axis.title.y = element_text(size = 12L),
+                  axis.title.x = element_text(size = 12L)
+                )
+#plot 2 plots into 1 graphic
+inweek_hours + weekend_hours
+
+#----------------------------------------------------------
+# Visualization 4: Number of Fatality by Age Group of Drivers
+#----------------------------------------------------------
+# filter only age groups rows with road users are drivers
+driver_ag <- fatality_accidents %>%
+            filter(Road_User_Type_Desc %in% 
+                     "Drivers") %>% 
+            filter(!(Age_Group %in% c("13-15", "unknown")))
+            
+driver_ag %>%  
+  ggplot() +
+  aes(x = Age_Group) +
+  geom_bar(fill = "#B22222") +
+  labs(
+    x = "Age Group",
+    y = "# of death",
+    title = "Number of Fatality By Age Groups of Driver"
+  ) +
+  ggthemes::theme_tufte() +
+  theme(
+    plot.title = element_text(size = 15L,
+                              face = "bold",
+                              hjust = 0.5),
+    plot.subtitle = element_text(size = 10L,
+                                 hjust = 0.5),
+    axis.title.y = element_text(size = 12L),
+    axis.title.x = element_text(size = 12L)
   )
 
-# Accident by weekdays
-accidents %>% 
-  group_by(day_of_week = Day_Week_Description) %>% 
-  summarize(total_accidents=n_distinct(ACCIDENT_NO)) %>%
-  ggplot(aes(x=day_of_week, y=total_accidents)) +
-  labs(x = "Days of Week", y = "Total Accidents", title = "Total Accident By Weekdays") +
-  geom_bar(stat="identity", fill="steelblue")+
-  geom_text(aes(label=total_accidents), vjust=1.6, color="white", size=3.5)+
-  theme(plot.title = element_text(size = 25L, 
-                                  face = "bold", hjust = 0.5))+
-  ggthemes::theme_stata()
-
-# Accident Severity Proportion by Weather Condition
-accidents %>% 
-  group_by(Atmosph_Cond_Desc,Inj_Level_Desc) %>% 
-  summarize(total_accidents=n_distinct(ACCIDENT_NO)) %>%
-  mutate(freq = percent(total_accidents / sum(total_accidents))) %>%
-  ggplot(aes(x=Inj_Level_Desc, y=freq,fill=Atmosph_Cond_Desc, na.rm = TRUE)) +
-  geom_bar(stat="identity", position="dodge")+
-  ggtitle("Accident Severity Proportion by Weather") +
-  xlab("Accident Severity") + ylab("Accident Proportion")+
-  theme(plot.title = element_text(hjust = 0.5), panel.background = element_blank(),
-        axis.text.y=element_blank(),axis.ticks.y=element_blank())
-
-
-# Accident Severity by weekdays
-accidents %>%
+#----------------------------------------------------------
+# Visualization 5: Speed Zone Vs. Other Environmental Factors
+#----------------------------------------------------------
+# light conditions
+light_conditions <- fatality_accidents %>%
+  filter(!(SPEED_ZONE %in% c("075", "999", "888", "777"))) %>%
+  filter(!(Light_Condition_Desc %in% 
+             c("Dark Street lights unknown", "Unknown"))) %>%
   ggplot() +
-  aes(x = Day_Week_Description, fill = Inj_Level_Desc) +
-  geom_bar(position = "dodge") +
-  scale_fill_manual(values = c(Fatality = "#0D0887", `Not injured` = "#7D06A5", `Other injury` = "#CA4778", 
-                               `Serious injury` = "#F69440", Unknown = "#F0F921")) +
-  labs(x = "Hours", y = "Total Accidents", title = "Accident Severity Levels By Weekdays", 
-       fill = "Severity Level") +
-  ggthemes::theme_stata() +
-  theme(plot.title = element_text(size = 25L, 
-                                  face = "bold", hjust = 0.5))
-
-accidents$ACCIDENT_HOUR <- format(strptime(accidents$ACCIDENTTIME, "%H:%M"), "%H:00")
-
-# Accident by hours
-accidents %>% 
-  group_by(ACCIDENT_HOUR) %>% 
-  summarize(total_accidents=n_distinct(ACCIDENT_NO)) %>%
-  ggplot(aes(x=ACCIDENT_HOUR, y=total_accidents)) +
-  geom_bar(stat="identity", fill="steelblue")+
-  geom_text(aes(label=total_accidents), vjust=1.6, color="black", size=3)+
-  ggtitle("Total Accidents by Hours") +
-  xlab("Hours") + ylab("Total Accidents")+
-  theme(plot.title = element_text(hjust = 0.5), panel.background = element_blank(), axis.text.x=element_text(angle = 90, vjust = 0.5))+
-  theme(plot.title = element_text(size = 25L, 
-                                  face = "bold", hjust = 0.5))
-
-# HeatMap Weekday vs Hours
-
-ggplot(accidents) +
-  aes(
-    x = Day_Week_Description,
-    y = ACCIDENT_HOUR,
-    fill = Inj_Level_Desc
-  ) +
-  geom_tile(size = 1.2) +
-  scale_fill_manual(
-    values = c(Fatality = "#800026",
-               `Not injured` = "#FED976",
-               `Other injury` = "#FD8D3C",
-               `Serious injury` = "#E31A1C",
-               Unknown = "#FFFFFF")
-  ) +
+  aes(x = Light_Condition_Desc, fill = SPEED_ZONE) +
+  geom_bar(position = "fill") +
+  scale_fill_brewer(palette = "OrRd", direction = 1) +
   labs(
-    x = "Days of Week",
-    y = "Hours",
-    title = "HeatMap Weekday vs Hours",
-    fill = "Severity Level"
+    x = "light conditions",
+    y = "percentage",
+    title = "Speed Zone Vs. Light Conditions"
   ) +
   coord_flip() +
-  ggthemes::theme_fivethirtyeight() +
+  theme_minimal() +
   theme(
-    plot.title = element_text(size = 20L,
+    plot.title = element_text(size = 15L,
+                              face = "bold",
+                              hjust = 0.5),
+    legend.position = "None"
+  )
+# weather
+weather <- fatality_accidents %>%
+  filter(!(SPEED_ZONE %in% c("075", "999", "888", "777"))) %>%
+  filter(!(Atmosph_Cond_Desc %in% 
+             "Not known")) %>%
+  ggplot() +
+  aes(x = Atmosph_Cond_Desc, fill = SPEED_ZONE) +
+  geom_bar(position = "fill") +
+  scale_fill_brewer(palette = "OrRd", direction = 1) +
+  labs(
+    x = "weather conditions",
+    y = "percentage",
+    title = "Speed Zone Vs. Weather Conditions"
+  ) +
+  coord_flip() +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 15L,
                               face = "bold",
                               hjust = 0.5)
-  )
-
-
-
+  ) 
+#plot 2 plots into 1 graphic
+light_conditions + weather
